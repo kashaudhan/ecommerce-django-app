c6e93c559a52ff2c9d9d65dfc263bf891314d136
dharmendra <dharmendra@thesolarlabs.com>
2021-07-19 13:53:51 +0530

    view products by category

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls.conf import include
 from greatkart import settings
 from django.contrib import admin
 from django.urls import path
@@ -22,5 +23,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('store/', include('store.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
